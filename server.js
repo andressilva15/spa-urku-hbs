3861c5058f1a54595e2a0d861e02c4267f93244e
andressilva15 <randressilva@gmail.com>
2018-11-23 23:28:51 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require ('./hbs/helpers');
+
+const port = process.env.PORT || 3200;
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/parciales');
+hbs.registerPartials(__dirname + '/views');
+
+
+app.set('view engine','hbs');
+app.get('/', (req,res) =>{
+  res.render('home',{
+    nombre:  'Carla, Leandro y Andres'
+  });
+});
+app.get('/about', (req,res)=>{
+  res.render('about');
+});
+app.get('/blog', (req,res)=>{
+  res.render('blog')
+});
+app.get('/contact', (req,res)=>{
+  res.render('contact')
+});
+app.get('/portfolio',(req,res)=>{
+  res.render('portfolio')
+});
+app.listen(port, () => {
+  console.log(`Escuchando peticiones en el puerto + ${port}`);
+});
